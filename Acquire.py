d934a256a6a3c8028c36641380f07e34cf25c588
Bethany Thompson <thompson.bethany.01@gmail.com>
2020-11-19 10:51:23 -0600

    Add Web Scraping Function to Scquire

--- Acquire.py
+++ Acquire.py
@@ -8,40 +8,91 @@ import os
 
 ######################################## Acquire URLS ########################################
 
-def acquire_repo_urls(language, period):
+# option for period is 
+# daily, weekly, monthly
+
+# enhancing the function to take a list of language and timeline as input and return lists of url and
+# and a list of language
+
+def get_top_repo(language_list, timeline):
+
     '''
-    Acquire URL's of GitHub's Most Trending repositories in English and filtered by coding 
-    language and time period of choice. Period can be daily, weekly, or monthly. Top
-    coding languages include Python, Javascript, and Swift. Returns a list of 25 URLS.
+    Creates a list of repository urls, 25 for each url specified at the given time period of daily,
+    weekly, or monthly. Returns the urls in a df with the coding language catagory and the url.
     '''
-    headers = {'User-Agent': 'GitHub'}
+    repo_links = []
+    language=[]
+    for element in language_list:
+        
+        url = f'https://github.com/trending/{element}?since={timeline}&spoken_language_code=en'
+        response = get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        h1s = soup.find_all('h1', class_ = 'h3 lh-condensed')
+        
+        
+        for elem  in h1s:
+            repo_name = elem.get_text()
+            repo_name = repo_name.replace('\n', '')
+            repo_name = repo_name.replace(' ', '')
+            links = 'https://github.com/' + repo_name
+            repo_links.append(links)
+            item= element
+            language.append(item)
 
-    # URL of GitHub trending repo page with langauge and period specified
-    response = get(f'https://github.com/trending/{language}?since={period}&spoken_language_code=en', headers=headers)
+    df = pd.DataFrame(repo_links, language).reset_index().rename(columns = {'index': 'language', 0:'link'})
+            
+    return df
+
+##################################### Web Scraping #####################################
+
+# creating a function that automates the parsing process given a list of links to parse
+def get_content_df(links):
+
+    '''Takes in a list of urs and parses through everyone of them to return a df of the 
+    repo associated with the url and its Readme file, watcher count, star count, and 
+    forks count.
+    '''
     
-    # parse html text
-    soup = BeautifulSoup(response.content, 'html.parser')
+    # empty list to hold feadme file content
+    content = []
+    # empty list to hold counts of user watchers
+    watchers = []
+    # empty list to hold counts of user stars
+    stars = []
+    # empty list to hold counts of user forks
+    forks = []
     
-    # all html text from trending page with h1 heading
-    h1 = soup.find_all('h1', class_='h3 lh-condensed')
-
-    # empty list to hold urls
-    repo_urls = []
-
-    # looping through all headers in h1
-    for h in h1:
-
-        # converting name to normalized text, no backspaces, spaces
-        repo_name = h.get_text()
-        repo_name = repo_name.replace("\n","")
-        repo_name = repo_name.replace(" ","")
-
-        # adding github url to complete the url
-        repo_name = 'https://github.com/' + repo_name
-
-        # adding one url to list of urls
-        repo_urls.append(repo_name)
+    # looping through each url generated from function above
+    for url in links:
+        
+        # empty list to hold individual repo ['watchers','stars','forks']
+        counts = []
+        
+        # scraping for readme content
+        response = get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        article = soup.find('article', itemprop = 'text')
+        article_text = article.get_text()
+        item = {
+        'content': article_text
+        }
+        content.append(item)
+        
+        # scraping for watcher, star, fork counts
+        text = soup.find_all('a', class_='social-count')
+        for h in text:
+            #['watchers','stars','forks']
+            counts.append(h.get_text().replace('\n','').replace(' ',''))
+        
+        # splitting counts to appropriate list
+        watchers.append(counts[0])
+        stars.append(counts[1])
+        forks.append(counts[2])
     
-    return repo_urls
-
-    ##################################### Web Scraping #####################################
+    # creating content into a df
+    df = pd.DataFrame(content)
+    df['watchers'] = watchers
+    df['starts'] = stars
+    df['forks'] = forks
+    
+    return df
