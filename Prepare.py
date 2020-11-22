1462889162a689fb8b62adae4c47081cf8604fd0
Bethany Thompson <thompson.bethany.01@gmail.com>
2020-11-22 14:23:12 -0600

    Acquire More Data

--- Prepare.py
+++ Prepare.py
@@ -104,14 +104,24 @@ def train_validate(df):
     # Import to use split function, can only split two at a time
     from sklearn.model_selection import train_test_split
 
-    # split into train + validate together and test by itself
+    # Frist, split into train + validate together and test by itself
+    # Test will be about %10 of the data, train + validate is %70 for now
     # Set random_state so we can reproduce the same 'random' data
-    train, validate = train_test_split(df, test_size = .32, random_state = 123)
+    train_validate, test = train_test_split(df, test_size = .10, random_state = 123)
+
+    # Second, we plit train + validate into their seperate variables
+    # Train will be about %70 of the data, Validate will be about %20 of the data
+    train, validate = train_test_split(train_validate, test_size = .30, random_state = 123)
+
+    # These two print functions allow us to ensure the date is properly split
+    # Will print the shape of each variable when running the function
+    print("train shape: ", train.shape, ", validate shape: ", validate.shape, ", test shape: ", test.shape)
 
     # Will print the shape of eachvariable as a percentage of the total data set
     # Varialbe to hold the sum of all rows (total observations in the data)
     total = df.count()[0]
     print("\ntrain percent: ", round(((train.shape[0])/total),2) * 100, 
-            ", validate percent: ", round(((validate.shape[0])/total),2) * 100, )
+            ", validate percent: ", round(((validate.shape[0])/total),2) * 100, 
+            ", test percent: ", round(((test.shape[0])/total),2) * 100)
 
-    return train, validate
+    return train, validate, test
